8353696ce5bdea413cee277cacd372e8d285a89e
Excla1mer <47521554+Excla1mer@users.noreply.github.com>
2019-03-18 15:23:08 +0700

    Add files via upload

--- lab2.1/main.c
+++ lab2.1/main.c
@@ -2,8 +2,127 @@
 
 int main()
 {
-	int value=0;
+	int a,b,c,x,d,e,f,h;
+	int value;
+	int *value0 = &e;
+	int *value1 = &f;
 	sc_memoryInit();
+        sc_regInit();
+	while(1)
+	{
+		printf("|1|Test mem |2|Test File |3|Test flag |4| Test Encode/Decode |5| Show memory |6| Show register\n");
+		scanf("%d",&a);
+		switch(a)
+		{
+			case 1:
+				do
+				{
+					printf("Select <cell> and <value>\n");
+					scanf("%d %d",&b,&c);
+					sc_memorySet(b,c);
+					printf("Repeat?\n");
+					scanf("%d",&x);
+				}while(x==1);
+
+				do
+                                {
+                                        printf("Select cell->value\n");
+                                        scanf("%d",&b);
+					if(b>=0&&b<=99)
+					{
+                                        	sc_memoryGet(b,&value);
+						printf("%d\n",value);
+					}
+					else
+					{
+						printf("Wrong cell!\n");
+					}
+                                        printf("Repeat?\n");
+                                        scanf("%d",&x);
+                                }while(x==1);
+				break;
+			case 2:
+				printf("|1| Save |2| Load\n");
+				scanf("%d",&d);
+				switch(d)
+				{
+					case 1:
+						sc_memorySave("file.txt");
+						break;
+					case 2:
+						sc_memoryLoad("file.txt");
+						break;
+					default :
+						printf("Wrong value!\n");break;
+
+				}
+				break;
+			case 3:
+				do
+                                {
+                                        printf("Select <register> and <value>\n");
+                                        scanf("%d %d",&b,&c);
+					if(b>=0&&b<=4)
+                                        {
+                                                sc_regSet(b,c);
+                                        }
+                                        else
+                                        {
+                                                printf("Wrong register!\n");
+                                        }
+                                        printf("Repeat?\n");
+					scanf("%d",&x);
+                                }while(x==1);
+
+				do
+                                {
+                                        printf("Select register->value\n");
+                                        scanf("%d",&b);
+					if(b>=0&&b<=4)
+					{
+                                        	sc_regGet(b,&value);
+						printf("%d\n",value);
+					}
+					else
+					{
+						printf("Wrong register!\n");
+					}
+                                      	printf("Repeat?\n");
+					scanf("%d",&x);
+                                }while(x==1);
+				break;
+			case 4:
+				printf("Choose command and operand\n");
+				scanf("%d%d",&b,&c);
+				sc_commandEncode(b,c,&value);
+				printf("%x\n",value);
+				/*int z=value;
+				sc_commandDecode(z,value,value0);
+				printf("command=%x operand=%x",value,*value0);*/
+				break;
+			case 5:
+				for(int i=0;i<10;i++)
+				{
+					for(int j=0;j<10;j++)
+					{
+						printf("%4d",sc_memory[i*10+j]);
+					}
+					printf("\n");
+				}
+				break;
+			default :
+				printf("Wrong value!\n");break;
+			case 6:
+				printf("\nRegister: ");
+    				for(int i=0; i <5; i++) {
+    				    sc_regGet(i,&value);
+    				    printf("[%d]%d ", i, value );
+   				 }
+    				printf("\n\n");
+				break;
+		}
+	}
+	/*sc_memoryInit();
 	for(int i = 0; i < 100; i++)
 		sc_memorySet(i,i);
 	sc_memoryGet(5,&value);
@@ -26,6 +145,7 @@ int main()
 	sc_regGet(2, &value);
 	printf("%d\n",value);
 	sc_regGet(7, &value);
-	printf("%d\n",sc_regGet(7, &value));
+	printf("%d\n",sc_regGet(7, &value));*/
 	return 0;
 }
+
